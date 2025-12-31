15a029d4706498609dd1a0391e3d74c06aebae21
abozam <robloxboy200809@gmail.com>
2025-12-31 04:25:38 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-```python
 import os
 import json
 import time
@@ -179,4 +178,3 @@ def home():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-```
